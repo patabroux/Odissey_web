c5e678afb081afaeef1d2cf58e7480e34a73d080
gor-arnaud <arnaud.michoud@gameofroom.fr>
2019-08-13 18:41:30 +0200

    Détection de la proximité des waypoints

--- scripts/config.js
+++ scripts/config.js
@@ -1,14 +1,12 @@
 config = {
     geolua : {
-        url : "https://geolua.com",
-        restApiPrefix : "api/v1/game",
-        eventApiSuffix : "event",
-        apiKey : "548A276B58A2C625A558099C90256EF9"
+        url : "https://odissey-geolua-proxy.herokuapp.com/game/"
     },
     leaflet: {
         startPos : [45.721314, 4.827577]
     },
     waypoints: {
+        proximityRadius : 0.02,
         baseUrl : "https://evening-oasis-86693.herokuapp.com/places"
         //baseUrl : "http://localhost:3000/places"
     }
